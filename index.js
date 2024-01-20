8fc2a661d192ca869867c3bbcfc7f13e65bae7e7
Gaspar Tovar <gtovar333@gmail.com>
2024-01-19 22:52:04 -0500

    Add compression function

--- index.js
+++ index.js
@@ -1,66 +1,73 @@
 var exec = require('child_process').exec;
+var fs = require('fs/promises');
+var path = require('path');
+var { promisify } = require('util');
+var execPromise = promisify(exec);
 
-var create = function() {
+var cwd = process.cwd();
+
+var create = function () {
   return new gs();
 };
 
-var gs = function() {
+var gs = function () {
   this.options = [];
   this._input = null;
-  this._executablePath = 'gs'; // Default to 'gs', can be overridden with setExecutablePath
+  this._executablePath = 'gs'; // Default to 'gs'
+  this.tempFolder = path.join(cwd, "temp");
 };
 
-gs.prototype.batch = function() {
+gs.prototype.batch = function () {
   this.options.push('-dBATCH');
   return this;
 };
 
-gs.prototype.device = function(device) {
+gs.prototype.device = function (device) {
   device = device || 'jpeg';
   this.options.push('-sDEVICE=' + device);
   return this;
 };
 
 // Method to set the Ghostscript executable path
-gs.prototype.setExecutablePath = function(path) {
+gs.prototype.setExecutablePath = function (path) {
   this._executablePath = path;
   return this;
 };
 
-gs.prototype.exec = function(callback) {
+gs.prototype.exec = function (callback) {
   var self = this;
 
   if (!this._input) return callback("Please specify input");
 
   var args = this.options.concat([this._input]).join(' ');
-  exec(this._executablePath + ' ' + args, function(err, stdout, stderr) {
+  exec(this._executablePath + ' ' + args, function (err, stdout, stderr) {
     callback(err, stdout, stderr);
   });
 };
 
-gs.prototype.input = function(file) {
+gs.prototype.input = function (file) {
   this._input = file;
   return this;
 };
 
-gs.prototype.jpegq = function(value) {
+gs.prototype.jpegq = function (value) {
   value = value || 75;
   this.options.push('-dJPEGQ=' + value);
   return this;
 };
 
-gs.prototype.dsfactor = function(value) {
+gs.prototype.dsfactor = function (value) {
   value = value || 1;
   this.options.push('-dDownScaleFactor=' + value);
   return this;
 };
 
-gs.prototype.nopause = function() {
+gs.prototype.nopause = function () {
   this.options.push('-dNOPAUSE');
   return this;
 };
 
-gs.prototype.output = function(file) {
+gs.prototype.output = function (file) {
   file = file || '-';
   this.options.push('-sOutputFile=' + file);
   if (file === '-') return this.quiet();
@@ -69,16 +76,40 @@ gs.prototype.output = function(file) {
 
 gs.prototype.q = gs.prototype.quiet;
 
-gs.prototype.quiet = function() {
+gs.prototype.quiet = function () {
   this.options.push('-dQUIET');
   return this;
 };
 
-gs.prototype.resolution = function(xres, yres) {
+gs.prototype.resolution = function (xres, yres) {
   this.options.push('-r' + xres + (yres ? 'x' + yres : ''));
   return this;
 };
 
 gs.prototype.r = gs.prototype.res = gs.prototype.resolution;
+
+// New method specifically for PDF compression
+gs.prototype.compressPdf = async function (base64Input) {
+  const hasTempFolder = fs.existsSync(this.tempFolder);
+  if (!hasTempFolder) {
+    await fs.mkdir(this.tempFolder);
+  }
+
+  const inputFilePath = path.join(this.tempFolder, "original.pdf");
+  const outputFilePath = path.join(this.tempFolder, "compressed.pdf");
+  await fs.writeFile(inputFilePath, base64Input, "base64");
+
+  this.batch().nopause().quiet().device('pdfwrite').output(outputFilePath).input(inputFilePath);
+
+  var args = this.options.concat([this._input]).join(' ');
+  await execPromise(this._executablePath + ' ' + args);
+
+  const compressedFileBase64 = await fs.readFile(outputFilePath, "base64");
+  await fs.unlink(inputFilePath);
+  await fs.unlink(outputFilePath);
+
+  return compressedFileBase64;
+};
+
 
 module.exports = create;
